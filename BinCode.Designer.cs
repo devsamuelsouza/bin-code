d0a7abf68dbcf4384fbe63e894b275409ac85a1e
Samuel Souza <silva.samueldev@gmail.com>
2025-05-25 14:29:09 -0300

    :bulb: Ok

---
+++ BinCode.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BinCode
+{
+    partial class Bincode
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bincode));
+            Title = new Label();
+            Painel = new Panel();
+            Converter = new Button();
+            To = new Label();
+            Select2 = new ComboBox();
+            Select1 = new ComboBox();
+            Output = new TextBox();
+            Input = new TextBox();
+            Painel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Title
+            // 
+            Title.Anchor = AnchorStyles.Top;
+            Title.AutoSize = true;
+            Title.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Title.Location = new Point(260, 28);
+            Title.Name = "Title";
+            Title.Size = new Size(166, 25);
+            Title.TabIndex = 0;
+            Title.Text = "Tradutor Binario";
+            // 
+            // Painel
+            // 
+            Painel.Controls.Add(Converter);
+            Painel.Controls.Add(To);
+            Painel.Controls.Add(Select2);
+            Painel.Controls.Add(Select1);
+            Painel.Controls.Add(Output);
+            Painel.Controls.Add(Input);
+            Painel.Location = new Point(12, 85);
+            Painel.Name = "Painel";
+            Painel.Size = new Size(662, 269);
+            Painel.TabIndex = 1;
+            // 
+            // Converter
+            // 
+            Converter.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Converter.Location = new Point(284, 240);
+            Converter.Name = "Converter";
+            Converter.Size = new Size(92, 26);
+            Converter.TabIndex = 2;
+            Converter.Text = "Converter";
+            Converter.UseVisualStyleBackColor = true;
+            Converter.Click += Converter_Click;
+            // 
+            // To
+            // 
+            To.AutoSize = true;
+            To.Font = new Font("Microsoft JhengHei", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            To.Location = new Point(314, 18);
+            To.Name = "To";
+            To.Size = new Size(35, 17);
+            To.TabIndex = 5;
+            To.Text = "Para";
+            // 
+            // Select2
+            // 
+            Select2.DisplayMember = "Texto";
+            Select2.DropDownStyle = ComboBoxStyle.DropDownList;
+            Select2.Font = new Font("Microsoft Sans Serif", 9.75F);
+            Select2.FormattingEnabled = true;
+            Select2.ItemHeight = 16;
+            Select2.Items.AddRange(new object[] { "Binario", "Texto" });
+            Select2.SelectedIndex = 0;
+            Select2.Location = new Point(355, 14);
+            Select2.Name = "Select2";
+            Select2.Size = new Size(113, 24);
+            Select2.TabIndex = 4;
+            Select2.Tag = "";
+            // 
+            // Select1
+            // 
+            Select1.DisplayMember = "Texto";
+            Select1.DropDownStyle = ComboBoxStyle.DropDownList;
+            Select1.Font = new Font("Microsoft Sans Serif", 9.75F);
+            Select1.FormattingEnabled = true;
+            Select1.ItemHeight = 16;
+            Select1.Items.AddRange(new object[] { "Binario", "Texto" });
+            Select1.SelectedIndex = 1;
+            Select1.Location = new Point(194, 14);
+            Select1.Name = "Select1";
+            Select1.Size = new Size(113, 24);
+            Select1.TabIndex = 3;
+            Select1.Tag = "";
+            Select1.SelectedValueChanged += Select1_SelectedValueChanged;
+            // 
+            // Output
+            // 
+            Output.Font = new Font("Microsoft Sans Serif", 9.75F);
+            Output.Location = new Point(348, 54);
+            Output.Multiline = true;
+            Output.Name = "Output";
+            Output.PlaceholderText = "Saída...";
+            Output.Size = new Size(254, 174);
+            Output.TabIndex = 1;
+            Output.KeyPress += Output_KeyPress;
+            // 
+            // Input
+            // 
+            Input.Font = new Font("Microsoft Sans Serif", 9.75F);
+            Input.Location = new Point(61, 54);
+            Input.Multiline = true;
+            Input.Name = "Input";
+            Input.PlaceholderText = "Entrada...";
+            Input.Size = new Size(254, 174);
+            Input.TabIndex = 0;
+            Input.KeyPress += Input_KeyPress;
+            // 
+            // Bincode
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(686, 390);
+            Controls.Add(Painel);
+            Controls.Add(Title);
+            Font = new Font("Microsoft Sans Serif", 8.25F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(702, 429);
+            MinimumSize = new Size(702, 429);
+            Name = "Bincode";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BinCode";
+            Painel.ResumeLayout(false);
+            Painel.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Title;
+        private Panel Painel;
+        private TextBox Output;
+        private TextBox Input;
+        private ComboBox Select1;
+        private ComboBox Select2;
+        private Label To;
+        private Button Converter;
+    }
+}
---
+++ Bincode.cs
@@ -0,0 +1,86 @@
+namespace BinCode
+{
+    public partial class Bincode : Form
+    {
+        public Bincode()
+        {
+            InitializeComponent();
+        }
+
+        private void Converter_Click(object sender, EventArgs e)
+        {
+            if (Select1.Text == Select2.Text)
+            {
+                string message = $"Não é possivel converter de {Select1.Text} para {Select2.Text}!";
+                MessageBox.Show(message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (Select1.Text == "Texto")
+            {
+                var output = "";
+                foreach (char c in Input.Text)
+                {
+                    string binario = Convert.ToString(c, 2).PadLeft(8, '0');
+                    output += binario + " ";
+                }
+                Output.Text = output.ToString();
+                return;
+            }
+            else
+            {
+                try
+                {
+                    string output = "";
+                    string binario = Input.Text.Replace(" ", "");
+
+                    for (int i = 0; i <= binario.Length - 8; i += 8)
+                    {
+                        string byteString = binario.Substring(i, 8); 
+                        int asciiCode = Convert.ToInt32(byteString, 2); 
+                        char letra = (char)asciiCode; 
+                        output += letra;
+                    }
+
+                    Output.Text = output;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        private void Select1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (Select1.Text == "Texto")
+            {
+                Select2.Text = "Binario";
+                Input.Text = string.Empty;
+                Output.Text = string.Empty;
+            }
+            else
+            {
+                Select2.Text = "Texto";
+                Input.Text = string.Empty;
+                Output.Text = string.Empty;
+            }
+        }
+
+        private void Input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Select1.Text == "Binario")
+            {
+                if (e.KeyChar != 0 && e.KeyChar != 1 && e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != ' ' && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void Output_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
